7050a57b8ee8f47876e4b3a874694e2301be2840
Samuel David Roncal Vidal <davrv93@gmail.com>
2022-10-13 19:08:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,37 @@ app.get('/oauth', (req, res) => {
 
     let url = 'https://www.tiktok.com/auth/authorize/';
 
-    url += '?client_key={CLIENT_KEY}';
+    url += '?client_key=awtfqqxrml4ih11d';
     url += '&scope=user.info.basic,video.list';
     url += '&response_type=code';
-    url += '&redirect_uri={SERVER_ENDPOINT_REDIRECT}';
+    url += '&redirect_uri=https://davrv93.github.io/tiktokauth/oauth';
     url += '&state=' + csrfState;
 
     res.redirect(url);
+})
+
+
+app.get('/callback', async (req, res) => {
+
+
+    if (!validateCsrfToken(req.query.state)) {
+        throw new Error("invalid csrf token");
+    }
+
+    async function getAccessTokenAndOpenId(code, TIKTOK_CLIENT_KEY, TIKTOK_CLIENT_SECRET) {
+        let urlAccessToken = 'https://open-api.tiktok.com/oauth/access_token/';
+        urlAccessToken += '?client_key=' + TIKTOK_CLIENT_KEY;
+        urlAccessToken += '&client_secret=' + TIKTOK_CLIENT_SECRET;
+        urlAccessToken += '&code=' + code;
+        urlAccessToken += '&grant_type=authorization_code';
+        const resp = await axios.post(urlAccessToken);
+        return {
+            accessToken: resp.data.data.access_token,
+            openId: resp.data.data.open_id,
+        };
+    }
+
+    const code = req.query.code;
+    const { openId, accessToken } = await getAccessTokenAndOpenId(code, TIKTOK_CLIENT_KEY, TIKTOK_CLIENT_SECRET);
+
 })
